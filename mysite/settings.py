201eb4e9786ccd583ce012774c4c97f82487ed9d
Omar <mosjdju1@gmail.com>
2020-05-02 18:21:32 +0200

    CSS dsad

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_DIRS = [os.path.join(BASE_DIR, 'bolg/static')]
